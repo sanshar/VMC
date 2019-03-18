dfff6a1e55ea76493e4115de9671ceef96bf92fa
Iliya <iliya.sabzevari@gmail.com>
2019-03-18 10:19:09 -0600

    forgot to delete a print statement

--- VMC/linearMethod.h
+++ VMC/linearMethod.h
@@ -113,7 +113,6 @@ class LM
        rt = 0.0;
        grad.setZero(numVars);
 
-       cout << "hi" << endl;
        double acceptedFrac = getHessian(vars, grad, Hessian, Smatrix, E0, stddev, rt);
        write(vars);
 
